de0903502ee1b4b42f4c200f17609c8bd33a3e3c
Rüstem Yıldırım <rustem.yildirim@topkapidanismanlik.com.tr>
2022-12-13 15:48:16 +0300

    Add project files.

---
+++ ParsingOfNumbers/Program.cs
@@ -0,0 +1,33 @@
+﻿using ParsingOfNumbers.Service;
+using System;
+using System.Threading.Tasks;
+
+namespace ParsingOfNumbers
+{
+    public class Program
+    {
+        private readonly IParseService _parseService;
+        public Program(IParseService parseService)
+        {
+            _parseService = parseService;
+        }
+
+        public async Task<string> Parse(string text)
+        {
+            var respText = await _parseService.ParseTheText(text);
+            return respText;
+        }
+        static async Task Main(string[] args)
+        {
+            var unparsedText = "";
+
+            IParseService parseService = new ParseService();
+            var program = new Program(parseService);
+
+            var parsedText = await program.Parse(unparsedText);
+
+            Console.WriteLine(parsedText);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ParsingOfNumbers/Service/ParseService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParsingOfNumbers.Service
+{
+    public class ParseService : IParseService
+    {
+        public async Task<string> ParseTheText(string text)
+        {
+            //The parsing process will be here
+            var unparsedText = "Hello";
+            return unparsedText;
+        }
+    }
+}
